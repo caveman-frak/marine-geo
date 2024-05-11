52d33c3a478e13d0a67b8869fbb725e7a4e323f3
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-05-11 12:08:30 +0100

    build tailwind css

--- src/main/resources/tailwind/tailwind.config.js
+++ src/main/resources/tailwind/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: [ './src/main/resources/**/*.{jte,html}' ],
+  content: [ './src/main/resources/**/*.{jte,js,html}' ],
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
