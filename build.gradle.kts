baf4088ed9210e2f550baf83f55ba282f7ed0b6a
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-05-27 13:45:34 +0100

    try joop

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jooq.meta.jaxb.Logging
+
 plugins {
     id("marine.application-conventions")
     id("marine.tailwind-build")
@@ -8,6 +10,63 @@ dependencies {
     implementation(project(":wire"))
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("io.github.wimdeblauwe:htmx-spring-boot-thymeleaf:3.3.0")
+}
+
+jooq {
+    configuration {
+        logging = Logging.TRACE
+        generator {
+            database {
+                name = "org.jooq.meta.extensions.jpa.JPADatabase"
+                properties {
+                    // A comma separated list of Java packages, that contain your entities
+                    property {
+                        key = "packages"
+                        value = "uk.co.bluegecko.marine.geo.data.model," +
+                                "uk.co.bluegecko.marine.geo.data.convertor," +
+                                "uk.co.bluegecko.marine.geo.data.repository"
+                    }
+                    // Whether JPA 2.1 AttributeConverters should be auto-mapped to jOOQ Converters.
+                    // Custom <forcedType/> configurations will have a higher priority than these auto-mapped converters.
+                    // This defaults to true.
+                    property {
+                        key = "useAttributeConverters"
+                        value = "true"
+                    }
+                    // The default schema for unqualified objects:
+                    //
+                    // - public: all unqualified objects are located in the PUBLIC (upper case) schema
+                    // - none: all unqualified objects are located in the default schema (default)
+                    //
+                    // This configuration can be overridden with the schema mapping feature
+                    property {
+                        key = "unqualifiedSchema"
+                        value = "public"
+                    }
+                    // Whether JPA 2.1 AttributeConverters should be auto-mapped to jOOQ Converters.
+                    // Custom <forcedType/> configurations will have a higher priority than these auto-mapped converters.
+                    // This defaults to true.
+                    property {
+                        key = "useAttributeConverters"
+                        value = "true"
+                    }
+//                    property {
+//                        key = "hibernate.physical_naming_strategy"
+//                        value = "org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy"
+//                    }
+                }
+            }
+            generate {
+                isFluentSetters = true
+                isJavaTimeTypes = true
+            }
+            target {
+                packageName = "uk.co.bluegecko.marine.geo.jooq"
+                directory = "src/main/generated"
+                isClean = true
+            }
+        }
+    }
 }
 
 testing {
