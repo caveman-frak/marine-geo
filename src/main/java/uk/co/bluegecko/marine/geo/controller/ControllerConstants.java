a0d6b00e80e6083eb39c350c95d3971902aab14a
Caveman Frak <tim.pickard@bluegecko.co.uk>
2024-10-05 09:52:29 +0100

    migrate to external dependency versions (part 3) switch from opencsv to beanio

--- src/main/java/uk/co/bluegecko/marine/geo/controller/ControllerConstants.java
+++ src/main/java/uk/co/bluegecko/marine/geo/controller/ControllerConstants.java
@@ -1,11 +1,6 @@
 package uk.co.bluegecko.marine.geo.controller;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.http.MediaType.APPLICATION_XML;
-import static org.springframework.web.servlet.function.RequestPredicates.accept;
-
 import lombok.experimental.UtilityClass;
-import org.springframework.web.servlet.function.RequestPredicate;
 
 /**
  * Controller constants.
@@ -13,11 +8,7 @@ import org.springframework.web.servlet.function.RequestPredicate;
 @UtilityClass
 public class ControllerConstants {
 
-	public static final RequestPredicate ACCEPT_JSON = accept(APPLICATION_JSON);
-	public static final RequestPredicate ACCEPT_XML = accept(APPLICATION_XML);
-	public static final RequestPredicate ACCEPT_JSON_XML = accept(APPLICATION_JSON, APPLICATION_XML);
-
 	public static final String COUNTRY = "/country";
 	public static final String CODE = "/{code}";
-	public static final String ID = "/{code}";
+
 }
